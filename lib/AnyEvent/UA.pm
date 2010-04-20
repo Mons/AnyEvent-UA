ead9a0cd680a2120db62e88b6dc1a1f309fd1c63
mons <mons@cpan.org>
2010-04-20 05:28:03 +0400

    first working version

--- lib/AnyEvent/UA.pm
+++ lib/AnyEvent/UA.pm
@@ -1,8 +1,25 @@
+package AnyEvent::UA::Req;
+
+sub new {
+	my $pk = shift;
+	my ($method,$uri) = (shift,shift);
+	my $self = bless {@_}, $pk;
+	$self->{method} = $method;
+	$self->{uri} = $uri;
+	$self;
+}
+
+sub error {
+	my $self = shift;
+	$self->{cb}( undef, HTTP::Easy::Headers->new->HTTP(@_) );
+	%$self = ();
+}
+
 package AnyEvent::UA::Con;
 
 sub new {
-	my $self = bless {},shift;
-	$self->{h} = shift;
+	my $pk = shift;
+	my $self = bless {@_}, $pk;
 	$self;
 }
 
@@ -126,7 +143,7 @@ sub decode_uri {
 	my $port = { http => 80, https => 443, }->{ lc $uri->scheme } or return;
 	my $realport = $uri->port;
 	my $host = lc $uri->host;
-	warn "$host : $port";
+	#warn "$host : $port";
 	my $host_header = $port != $realport ? "$host:$realport" : $host;
 	my $proxy;
 	my ($rhost, $rport, $rscheme, $rpath); # request host, port, path
@@ -150,10 +167,57 @@ sub req {
 	$uri->path('/') unless length $uri->path;
 	my %state;
 	my $e = sub { my ($code,$mess) = @_; %state = (); $args{cb}(undef, { Status => $code, Reason => $mess, URL => $uri }); };
+	
 	my ($host, $port, $scheme, $path, $host_header) = $self->decode_uri($uri)
 		or return $e->(599);
+	
 	my $headers  = HTTP::Easy::Headers->new( { %{$self->{headers}}, host => $host_header } );
 	warn "($host, $port, $scheme, $path) \n";
+	my $conkey = "$host:$port";
+	my $id;
+	if (exists $self->{ka}{$conkey}) {
+		$id = $self->{ka}{$conkey};
+		warn "Have KA for $conkey: $self->{con}{ $id }";
+		push @{ $self->{con}{ $id }{r} }, AnyEvent::UA::Req->new(
+			$method, $uri, %args, headers => $headers,
+		);
+		#$self->rr($h, $method, $uri, %args, path => $path, headers => $headers);
+		$self->rr2($id);
+		return;
+	} else {
+		# TODO:
+		# push req to r, connect, handle all
+		{
+			weaken( my $this = $self );
+			my $con = {
+				host => $host,
+				port => $port,
+				r   => [],
+				# TODO
+# 				$self->{keep_alive} ? (
+# 					ka  => AE::timer 300,0,sub {
+# 						$self or return;
+# 						delete $self->{con}{$id};
+# 					},
+# 				) : (),
+			};
+			$id = int $con;
+			$con->{id} = $id;
+			$self->{con}{$id} = $con;
+			$self->{ka}{$conkey} = $id;
+			$con->{close} = sub {
+				$this or return;
+				exists $this->{con}{$id} or return;
+				for(@{ $this->{con}{$id}{r} }) {
+					$_->error(599,$_[0]);
+				}
+				delete $this->{con}{$id};
+			};
+		}
+		push @{ $self->{con}{ $id }{r} }, AnyEvent::UA::Req->new(
+			$method, $uri, %args, headers => $headers,
+		);
+	}
 	my $timeout = $args{timeout} || $TIMEOUT;
 	my $proxy   = $args{proxy};#    || $PROXY;
 	$state{connect} =
@@ -165,47 +229,23 @@ sub req {
 				if (my $fh = shift) {
 					warn "connected 1";
 					return unless delete $state{connect};
-					my $id = int $fh;
 					warn "connected 2. id = $id";
 					my $h = AnyEvent::Handle->new(
 						fh       => $fh,
 						timeout  => $timeout,
 						peername => $host,
-						on_eof     => sub { warn "EOF";    delete $self->{con}{$id}; $e->(599, "Unexpected end-of-file") },
-						on_error   => sub { warn "ERR @_"; delete $self->{con}{$id}; $e->(599, $_[2]); },
+						on_eof     => sub { warn "EOF";    delete($self->{con}{$id})->{close}("Unexpected end-of-file") },
+						on_error   => sub { warn "ERR @_"; delete($self->{con}{$id})->{close}( $_[2]); },
 						#tls_ctx  => $arg{tls_ctx},
 					);
-					{
-						weaken( my $this = $self );
-						$self->{con}{$id} = {
-							fh  => $fh,
-							h   => $h,
-							r   => [],
-							# TODO
-		# 					$self->{keep_alive} ? (
-		# 						ka  => AE::timer 300,0,sub {
-		# 							$self or return;
-		# 							delete $self->{con}{$id};
-		# 						},
-		# 					) : (),
-						};
-					}
+					$self->{con}{$id}{h} = $h;
 
 					# TODO: limit KA conns
 					# (re-)configure handle
 					my $request = sub {
-						warn dumper \%state;
 						# Connection initially established
-						my $closeall = sub {
-							for(@{ $self->{con}{$id}{r} }) {
-								$_->[0](undef,$_[0]);
-							}
-							delete $self->{con}{$id};
-						};
-						$h->{on_eof}   = sub { warn "EOF2";$closeall->( "Unexpected end-of-file" )};
-						$h->{on_error} = sub { warn "ERR2 @_"; $closeall->($_[2]); };
-						#push @{ $self->{con}{$id}{r} };
-						$self->rr($h, $method, $uri, %args, path => $path, headers => $headers);
+						#$self->rr($h, $method, $uri, %args, path => $path, headers => $headers);
+						$self->rr2($id);
 					};#END $request
 					# now handle proxy-CONNECT method
 					$h->starttls ("connect") if $scheme eq "https";
@@ -217,15 +257,15 @@ sub req {
 								or return $e->(599, "Invalid proxy connect response ($_[1])");
 							if ($2 == 200) {
 								$path = $uri->path;
-								$self->{con}{$id}{type} = 'proxy';
-								&$request;
+								$self->{con}{$id}{type} = 'raw';
+								$self->rr2($id);
 							} else {
 								return $e->($2,$3);
 							}
 						});
 					} else {
 						$h->starttls ("connect") if $scheme eq "https" && !exists $state{handle}{tls};
-						&$request;
+						$self->rr2($id);
 					}
 				} else {
 					warn "Got error @_";
@@ -234,6 +274,145 @@ sub req {
 			}
 	);
 	return;
+}
+
+sub rr2 {
+	my $self = shift;
+	my $id = shift;
+	return warn "no such connection $id" unless exists $self->{con}{$id};
+	my $con = $self->{con}{$id};
+	return warn ("Not connected yet"), unless $con->{h};
+	return warn ("No more requests for $id"), $con->{h}->timeout(undef) unless @{ $con->{r} };
+	#while (@{ $con->{r} }) {
+		my $r = shift @{ $con->{r} };
+		warn "Run request $r->{method} $r->{uri} over $con->{id}";
+		if ($con->{type} eq 'proxy') {
+			return $r->error(599, "Proxy not implemented");
+		} else {
+			my $path = $r->{uri}->path_query;
+			$con->{h}->push_write (
+				"$r->{method} $path HTTP/1.1\015\012" .
+				$r->{headers}->encode . "\015\012" .
+				(delete $r->{body})
+			);
+			$con->{h}->push_read (line => $qr_nl, sub {
+				#return unless exists $self->{con}{$id};
+				$_[1] =~ /^HTTP\/([0-9\.]+) \s+ ([0-9]{3}) (?: \s+ ([^\015\012]*) )?/ixo
+					or return $r->error( 599, "Invalid server response ($_[1])" );
+					
+				my $status = $2;my $reason = $3;my $http_version = $1;
+
+				# headers, could be optimized a bit
+				$con->{h}->unshift_read (line => $qr_nlnl, sub {
+					my $uri = $r->{uri};
+					my $method = $r->{method};
+					my $hdr = HTTP::Easy::Headers->decode($_[1], base => $uri);
+					$hdr->{Status} = $status;
+					$hdr->{Reason} = $reason;
+					# TODO: check correctness?
+					#	or return $r->error(599, "Garbled response headers");
+					$self->{cookie}->decode($hdr->{"set-cookie"},  host => $uri->host) if exists $hdr->{"set-cookie"};
+					$self->{cookie}->decode($hdr->{"set-cookie2"}, host => $uri->host) if exists $hdr->{"set-cookie2"};
+					$self->{cookie}->decode($hdr->{"set-cookie3"}, host => $uri->host) if exists $hdr->{"set-cookie3"};
+
+					my $redirect;
+					my $recurse = 0;# TODO: exists $args{recurse} ? delete $args{recurse} : $MAX_RECURSE;
+
+					if ($recurse) {
+						if ($status =~ /^30[12]$/ and $method ne "POST") {
+							# apparently, mozilla et al. just change POST to GET here
+							# more research is needed before we do the same
+							$redirect = 1;
+						}
+						elsif ($status == 303) {
+							# even http/1.1 is unclear on how to mutate the method
+							$method = "GET" unless $method eq "HEAD";
+							$redirect = 1;
+						}
+						elsif ($status == 307 and $method =~ /^(?:GET|HEAD)$/) {
+							$redirect = 1;
+						}
+					}
+
+					my $finish = sub {
+						#$con->destroy if $con;
+						#%state = ();
+
+						if ($redirect && exists $hdr->{location}) {
+							# we ignore any errors, as it is very common to receive
+							# Content-Length != 0 but no actual body
+							# we also access %hdr, as $_[1] might be an erro
+							#http_request ($method => $hdr{location}, %arg, recurse => $recurse - 1, $cb);
+							warn "Redirect => $hdr->{location}";
+						} else {
+							$r->{cb}($_[0], $_[1]);
+						}
+						$self->rr2($id);
+					};
+
+					my $len = $hdr->{"content-length"};
+
+# 					if (!$redirect && $args{on_header} && !$args{on_header}($hdr)) {
+# 						$finish->(undef, { Status => 598, Reason => "Request cancelled by on_header", URL => $uri });
+# 					}
+# 					elsif (
+					if(
+						$status =~ /^(?:1..|[23]04)$/
+						or $method eq "HEAD"
+						or (defined $len && !$len)
+					) {
+						# no body
+						$finish->("", $hdr);
+					}
+					else {
+						#warn dumper $hdr,$self->{cookie};
+						if (lc $hdr->{'transfer-encoding'} eq 'chunked') {
+							my $body = '';
+							my $get_chunk;$get_chunk = sub {
+								$con->{h}->unshift_read( regex => qr{([a-f0-9]{1,32})(?:[\011\040]+[^\012]{0,255})?\015?\012}o,sub {
+									my $chunk = hex($1);
+									if ($chunk > 0) {
+										$get_chunk->();
+										#warn "need chunk $chunk";
+										$_[0]->unshift_read(chunk => $chunk, sub {
+											$body .= $_[1];
+										});
+									} else {
+										undef $get_chunk;
+										#warn "Got all chunks, read trailer";
+										$_[0]->unshift_read(line => $qr_nlnl, sub {
+											#warn "Got trailer $_[1]";
+											$finish->($body,$hdr);
+										});
+									}
+								});
+							};
+							$get_chunk->();
+						} else {
+							$_[0]->on_eof (undef);
+							if ($len) {
+								warn "ready for body (+$len)";
+								$_[0]->on_error (sub { $finish->(undef, $hdr->HTTP(599,$_[2])) });
+								$_[0]->unshift_read(chunk => $len, sub {
+									$finish->($_[1],$hdr);
+								});
+							} else {
+								warn "ready for body until eof";
+								$_[0]->on_error (sub {
+									$! == Errno::EPIPE || !$!
+										? $finish->(delete $_[0]{rbuf}, $hdr)
+										: $finish->(undef, $hdr->HTTP(599,$_[2]));
+								});
+								$_[0]->on_read (sub { });
+							}
+						}
+						
+					}
+				});
+			});
+			
+		}
+	#}
 }
 
 sub rr { # request/response
--- lib/AnyEvent/test.pl
+++ lib/AnyEvent/test.pl
@@ -6,11 +6,21 @@ use uni::perl ':dumper';
 
 my $ua = AnyEvent::UA->new();
 warn dumper $ua;
-# $ua->req(GET => 'HTTP://www.google.ru:80', cb => sub {
-# 
-# });
-$ua->req(GET => 'http://i.rl0.ru/search/logo_www.gif?v=10', cb => sub {
+$ua->req(GET => 'HTTP://www.google.ru:80', cb => sub {
+	warn dumper $_[1];
+	$ua->req(GET => 'HTTP://www.google.ru:80', cb => sub {
+		warn dumper $_[1];
+	});
+});
+$ua->req(GET => 'HTTP://www.google.ru:80', cb => sub {
 	warn dumper $_[1];
 });
+ 
+$ua->req(GET => 'http://i.rl0.ru/search/logo_www.gif?v=10', cb => sub {
+	warn dumper $_[1];
+	$ua->req(GET => 'http://i.rl0.ru/search/logo_www.gif?v=10', cb => sub {
+		warn dumper $_[1];
+	});
+}) if 0;
 
 $ua->{cv}->recv;
